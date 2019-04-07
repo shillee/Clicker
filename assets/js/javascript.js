ebd769371bf64f95aab4e3d7ff8b17eb70cc9845
shillee <serafim123semikhat@gmail.com>
2019-04-07 23:42:16 +0300

    123

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,55 +1,79 @@
-var blob = 0;
-<<<<<<< HEAD
-var autoClick = 0;
-=======
-var xM = 1;
-var cost = 50;
->>>>>>> 28667d3b9a0f7efdf0f3b16a268b8036adaf6aa9
-window.onload = function () {
-    blob = parseInt(blob);
-    blob = localStorage.getItem('bolbcunt');
-    document.getElementById('text').value = blob;
-    
-}
-<<<<<<< HEAD
-function update() {
-=======
+// var blob = 0;
+// var autoClick = 0;
+// var xM = 1;
+// var cost = 50;
+// window.onload = function () {
+//     blob = parseInt(blob);
+//     blob = localStorage.getItem('bolbcunt');
+//     document.getElementById('text').value = blob;
 
-function add() {
-    blob += xM;
-    localStorage.setItem('bolbcunt', blob);
->>>>>>> 28667d3b9a0f7efdf0f3b16a268b8036adaf6aa9
-    document.getElementById('text').value = blob;
-    document.title = blob + ' Blob';
-    document.getElementById('ammountAutoClick').innerHTML = 'You own ' + autoClick + ' Auto click';
-    setInterval(timer, 1000);
-}
+// }
 
-function upgradeCloud() {
-    if (blob >= cost) {
-        blob -= cost;
-        document.getElementById('text').value -= cost;
-        xM *= 2;    //Коэффициент увеличения эфективности прокачки (менять 2 на любое значение)
-        cost += (cost / 2 );  //Коэффициент увеличения стоимости прокачки (менять 2 на любое значение)
-        console.log(cost); //Цена прокачки твоего облака
-    } else {
-        alert("Недостаточно средств!");
+// function update() {
+
+//     function add() {
+//         blob += xM;
+//         localStorage.setItem('bolbcunt', blob);
+//         document.getElementById('text').value = blob;
+//         document.title = blob + ' Blob';
+//         document.getElementById('ammountAutoClick').innerHTML = 'You own ' + autoClick + ' Auto click';
+//         setInterval(timer, 1000);
+//     }
+
+//     function upgradeCloud() {
+//         if (blob >= cost) {
+//             blob -= cost;
+//             document.getElementById('text').value -= cost;
+//             xM *= 2; //Коэффициент увеличения эфективности прокачки (менять 2 на любое значение)
+//             cost += (cost / 2); //Коэффициент увеличения стоимости прокачки (менять 2 на любое значение)
+//             console.log(cost); //Цена прокачки твоего облака
+//         } else {
+//             alert("Недостаточно средств!");
+//         }
+//     }
+
+//     function timer() {
+//         blob = blob + autoClick;
+//         update()
+//     }
+
+
+//     function add() {
+//         blob++;
+//         localStorage.setItem('bolbcunt', blob);
+//         update()
+//     }
+
+
+    var blob = 0;
+    var xM = 1;
+    var cost = 50;
+    window.onload = function () {
+        blob = parseInt(blob);
+        blob = localStorage.getItem('bolbcunt');
+        document.getElementById('text').value = blob;
     }
-}
-function timer() {
-    blob = blob + autoClick;
-    update()
-}
 
+    function add() {
+        localStorage.setItem('bolbcunt', blob);
+        blob += xM;
+        document.getElementById('text').value = blob;
+        document.title = blob + ' Blob';
+    }
 
-function add() {
-    blob++;
-    localStorage.setItem('bolbcunt', blob);
-    update()
-}
+    function upgradeCloud() {
+        if (blob >= cost) {
+            blob -= cost;
+            document.getElementById('text').value -= cost;
+            xM *= (Math.floor(2)); 
+            cost += Math.floor(cost / 0.9);
+            console.log(cost);
+        }else{
+            alert('Недостаточно средств!');
+        }
+    }
+//     //Необходимо доделать запись переменных
+//     //xM
+//     //cost
+//     //в куки файлы//
 
-
-//Необходимо доделать запись переменных
-//xM
-//cost
-//в куки файлы
