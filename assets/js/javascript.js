ac3802fb73dac1206b9bb6e801f0460a053f2f17
shillee <serafim123semikhat@gmail.com>
2019-04-07 18:31:12 +0300

    v2

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,5 +1,14 @@
 var blob = 0;
-
+var autoClick = 0;
+function update() {
+    document.getElementById('text').value = blob;
+    document.title = blob + ' Blob';
+}
+function timer() {
+    blob = blob + autoClick;
+    update()
+}
+setInterval(timer, 1000);
 window.onload = function () {
     blob = parseInt(blob);
     blob = localStorage.getItem('bolbcunt');
@@ -10,5 +19,12 @@ function add() {
     localStorage.setItem('bolbcunt', blob);
     document.getElementById('text').value = blob;
     document.title = blob + ' Blob';
+}
+function buyAutoClick() {
+    if (blob >= ((autoClick+1) * 12)) {
+        blob = blob - ((autoClick+1) * 12);
+        autoClick = autoClick + 1;
+        update()
+    }
 }
 
