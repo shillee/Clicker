6fa8fe63991d5ce10c40d20c09282bf09f366964
Ник Чередник <777archie.nik777@gmail.com>
2019-04-07 20:48:32 +0300

    v2

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,30 +1,27 @@
 var blob = 0;
-var autoClick = 0;
-function update() {
-    document.getElementById('text').value = blob;
-    document.title = blob + ' Blob';
-}
-function timer() {
-    blob = blob + autoClick;
-    update()
-}
-setInterval(timer, 1000);
+var xM = 1;
+var cost = 50;
 window.onload = function () {
     blob = parseInt(blob);
     blob = localStorage.getItem('bolbcunt');
     document.getElementById('text').value = blob;
 }
+
 function add() {
-    blob++;
+    blob += xM;
     localStorage.setItem('bolbcunt', blob);
     document.getElementById('text').value = blob;
     document.title = blob + ' Blob';
 }
-function buyAutoClick() {
-    if (blob >= ((autoClick+1) * 12)) {
-        blob = blob - ((autoClick+1) * 12);
-        autoClick = autoClick + 1;
-        update()
+
+function upgradeCloud() {
+    if (blob >= cost) {
+        blob -= cost;
+        document.getElementById('text').value -= 50;
+        xM *= 2;
+        cost *= 2;
+        console.log(cost);
+    } else {
+        alert("Недостаточно средств!");
     }
 }
-
