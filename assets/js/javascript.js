24f5dad1c59c090729fad7ebe1fa8d492955cec6
shillee <serafim123semikhat@gmail.com>
2019-04-07 23:12:52 +0300

    123

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,24 +1,16 @@
 var blob = 0;
 var autoClick = 0;
-function update() {
-    document.getElementById('text').value = blob;
-    document.title = blob + ' Blob';
-}
-function timer() {
-    blob = blob + autoClick;
-    update()
-}
-setInterval(timer, 1000);
 window.onload = function () {
     blob = parseInt(blob);
     blob = localStorage.getItem('bolbcunt');
     document.getElementById('text').value = blob;
+    
 }
-function add() {
-    blob++;
-    localStorage.setItem('bolbcunt', blob);
+function update() {
     document.getElementById('text').value = blob;
     document.title = blob + ' Blob';
+    document.getElementById('ammountAutoClick').innerHTML = 'You own ' + autoClick + ' Auto click';
+    setInterval(timer, 1000);
 }
 function buyAutoClick() {
     if (blob >= ((autoClick+1) * 12)) {
@@ -27,4 +19,16 @@ function buyAutoClick() {
         update()
     }
 }
+function timer() {
+    blob = blob + autoClick;
+    update()
+}
+
+
+function add() {
+    blob++;
+    localStorage.setItem('bolbcunt', blob);
+    update()
+}
+
 
